6a46f1f9badd89d84cff74b81b4c78ef243a006a
binhhuynguyen22-hub <binhhuynguyen22@gmail.com>
2026-07-09 20:42:24 +0700

    Update menu_item_services.py

--- menu_item_services.py
+++ menu_item_services.py
@@ -1,16 +1,26 @@
 from sqlalchemy.orm import Session
 from schemas import CreateMenu, UpdateMenu
 from model import MenuItem
+from fastapi import HTTPException,status
 
 def create_menu_item_services(db:Session,new_menu:CreateMenu):
     new_menu = MenuItem(**new_menu.model_dump())
-    db.add(new_menu)
-    db.commit()
-    db.refresh(new_menu)
+    try:
+        db.add(new_menu)
+        db.commit()
+        db.refresh(new_menu)
 
-    return new_menu
+        return new_menu
+    except Exception as err:
+        db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(err)
+        )
 def get_all_menu_item_services(db:Session):
     list = db.query(MenuItem).all()
+    if not list:
+        return None
     return list
 def get_menu_item_by_id_services(db:Session, id: int):
     check = db.query(MenuItem).filter(MenuItem.id == id).first()
@@ -25,9 +35,16 @@ def update_menu_items_services(db: Session, id :int, update_menu:UpdateMenu):
         return None
     for key, value in update_menu.model_dump().items():
         setattr(update,key,value)
+    try:
         db.commit()
         db.refresh(update)
-    return update
+        return update
+    except Exception as err:
+            db.rollback()
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=str(err)
+            )
 def delete_menu_item_services(db:Session, id:int):
     delete = db.query(MenuItem).filter(MenuItem.id == id).first()
 
@@ -35,6 +52,13 @@ def delete_menu_item_services(db:Session, id:int):
 
     if delete is None:
         return None
-    db.delete(delete)
-    db.commit()
-    return menu_delete
+    try:
+        db.delete(delete)
+        db.commit()
+        return menu_delete
+    except Exception as err:
+        db.rollback()
+    raise HTTPException(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        detail=str(err)
+    )
